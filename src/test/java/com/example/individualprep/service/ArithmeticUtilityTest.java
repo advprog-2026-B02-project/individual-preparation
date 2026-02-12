def06f667fc16817e41cd9c96e6acb12d2b45875
mugmold <mugmold@gmail.com>
2026-02-12 21:07:27 +0700

    initial unit test

---
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
@@ -0,0 +1,34 @@
+package com.example.individualprep.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArithmeticUtilityTest {
+    private final ArithmeticUtility arithmetic = new ArithmeticUtility();
+
+    @Test
+    void testAdd() {
+        // TODO: Implement test for add(o1, o2)
+    }
+
+    @Test
+    void testSubtract() {
+        // TODO: Implement test for subtract(o1, o2)
+    }
+
+    @Test
+    void testMultiply() {
+        // TODO: Implement test for multiply(o1, o2)
+    }
+
+    @Test
+    void testDivide() {
+        // TODO: Implement test for divide(o1, o2)
+        // Hint: divide by zero!
+    }
+
+    @Test
+    void testExponent() {
+        // TODO: Implement test for exponent(o1, n)
+    }
+}
---
+++ src/test/java/com/example/individualprep/service/VectorUtilityTest.java
@@ -0,0 +1,33 @@
+package com.example.individualprep.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorUtilityTest {
+    private final VectorUtility vector = new VectorUtility();
+
+    @Test
+    void testAdd() {
+        // TODO: Implement test for add(v1, v2)
+    }
+
+    @Test
+    void testSubtract() {
+        // TODO: Implement test for subtract(v1, v2)
+    }
+
+    @Test
+    void testMultiply() {
+        // TODO: Implement test for multiply(v1, x)
+    }
+
+    @Test
+    void testDotProduct() {
+        // TODO: Implement test for dotProduct(v1, v2)
+    }
+
+    @Test
+    void testNorm() {
+        // TODO: Implement test for norm(v1)
+    }
+}
