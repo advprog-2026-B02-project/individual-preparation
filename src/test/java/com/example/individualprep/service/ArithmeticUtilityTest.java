c47d03a3b674c400449bd5899eb121274ae88f7a
mugmold <mugmold@gmail.com>
2026-02-12 21:26:28 +0700

    test: implement arithmetic multiply unit tests

--- src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTest.java
@@ -1,6 +1,7 @@
 package com.example.individualprep.service;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArithmeticUtilityTest {
@@ -18,7 +19,15 @@ class ArithmeticUtilityTest {
 
     @Test
     void testMultiply() {
-        // TODO: Implement test for multiply(o1, o2)
+        assertEquals(50.0, arithmetic.multiply(10.0, 5.0), "10 * 5 harus 50");
+
+        assertEquals(15.0, arithmetic.multiply(-3.0, -5.0), "-3 * -5 harus 15");
+        assertEquals(-15.0, arithmetic.multiply(3.0, -5.0), "3 * -5 harus -15");
+
+        assertEquals(0.0, arithmetic.multiply(0.0, 99.0), "0 * 99 harus 0");
+        assertEquals(0.0, arithmetic.multiply(99.0, 0.0), "99 * 0 harus 0");
+
+        assertEquals(0.25, arithmetic.multiply(0.5, 0.5), "0.5 * 0.5 harus 0.25");
     }
 
     @Test
